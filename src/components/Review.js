4bcd8ec0090736f71204e8750d91a6923ffb4d54
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2024-02-14 07:19:24 +0000

    check test cases

---
+++ src/components/Review.js
@@ -0,0 +1,97 @@
+import React, {useState} from "react";
+
+
+const reviewData= [
+    {
+      id: 1,
+      name: 'susan smith',
+      job: 'web developer',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: 'anna johnson',
+      job: 'web designer',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+      text:
+        'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    },
+    {
+      id: 3,
+      name: 'peter jones',
+      job: 'intern',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      text:
+        'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+      id: 4,
+      name: 'bill anderson',
+      job: 'the boss',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      text:
+        'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+  ];
+
+
+const Review= ()=>{
+
+    const [index,setIndex] = useState(0);
+    const {id,name,job,image,text}= reviewData[index];
+
+
+
+    function nextClick(){
+        
+        if(index< reviewData.length-1){
+            setIndex(index+1);
+        }
+        else{
+            return;
+        }
+    }
+
+    function prevClick(){
+
+       
+        if(index>0){
+            setIndex(index-1);
+        }
+        else{
+            return;
+        }
+    }
+
+    function randomClick(){
+        let num = Math.floor(Math.random()* (reviewData.length));
+        setIndex(num);
+        
+    }
+
+    return(
+        <div className="review">
+            <div>
+                <p author-id={id} className="author">{name}</p>
+                <p className="job">{job}</p>
+                <p className="info">{text}</p>
+                <img src={image} className="person-img"  />
+            </div>
+            <div>
+                <button onClick={prevClick} className="prev-btn">previous</button>
+                <button onClick={nextClick} className="next-btn">next</button>
+                <button onClick={randomClick} className="random-btn">random</button>
+            </div>
+
+        
+        </div>
+    )
+}
+
+export default Review
